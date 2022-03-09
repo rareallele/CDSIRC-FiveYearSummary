a0285e80557e26ee02d7796b5cf51a88e613459d
Jago Van Dam <jagovandam@hotmail.com>
2022-03-09 15:22:26 +1030

    add analysis and css, js

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function setIframe() {
+    var selection = document.getElementById("fig_select").value;
+    document.getElementById("figure-display").src = selection
+}
+
+
+function openPanel() {
+    var panel = document.getElementById("panel");
+    var acc = document.getElementsByClassName("accordion")[0]
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    acc.classList.toggle("active");
+    /* Toggle between hiding and showing the active panel */
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+}
